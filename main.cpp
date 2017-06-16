1a7fb90ef1cd414eecb9c78a825369eb6bebd308
Danji Cao <danji.cao@gmail.com>
2017-06-16 10:28:25 +0800

    Add conversion modes: hex2bin and bin2hex

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <iostream>
+#include <fstream>
 #include "config.h"
 #include "libhello/libhello.h"
 #include "libsysinfo/libsysinfo.h"
@@ -69,6 +70,113 @@ int PluginEventHandler( const char * ) {
 }
 #endif
 
+bool convertHex2Bin( const std::string & input, const std::string & output, bool verbose ) {
+    bool result = true;
+    const char digits[] = "0123456789abcdef";
+    std::ofstream ofs;
+    ofs.open(output.c_str(), std::ios_base::binary|std::ios_base::out|std::ios_base::trunc);
+    if ( ofs.is_open() ) {
+        printf("Open output file: %s\n", output.c_str());
+    }
+
+    size_t index=0;
+    for ( std::string::const_iterator ch = input.cbegin(); ch != input.cend(); ) {
+        unsigned char numU = 0, numV = 0;
+        const char * u = strchr(digits, tolower(*ch));
+        if ( u ) {
+            numU = u-digits;
+            if ( verbose ) {
+                printf("u[%2d]=%d\n", index, numU);
+            }
+        }
+        else {
+            result = false;
+            printf("Error! Conversion failed at %d (%c).\n", index, *ch);
+            break;
+        }
+        ++ch;
+        ++index;
+        const char * v = digits;
+        if ( ch != input.cend() ) {
+            v = strchr(digits, tolower(*ch));
+            if ( v ) {
+                numV = v-digits;
+                if ( verbose ) {
+                    printf("v[%2d]=%d\n", index, numV);
+                }
+            }
+            else {
+                result = false;
+                printf("Error! Conversion failed at %d (%c).\n", index, *ch);
+                break;
+            }
+            ++ch;
+            ++index;
+        }
+
+        unsigned char num = numU << 4 | numV;
+        if ( verbose ) {
+            printf("u x 16 + v = %d = 0x%02X\n", num, num);
+        }
+
+        if ( ofs.is_open() ) {
+            if ( verbose ) {
+                printf("Close output file: %s.\n", output.c_str());
+            }
+            ofs << (unsigned char) num;
+        }
+    }
+
+    if ( ofs.is_open() ) {
+        ofs.close();
+    }
+
+    return result;
+}
+
+
+bool convertBin2Hex( const std::string & input, const std::string & output, bool verbose ) {
+    bool result = true;
+    const char digits[] = "0123456789abcdef";
+    std::ifstream ifs(input.c_str(), std::ifstream::in);
+
+    if ( ifs.is_open() ) {
+        printf("Open input file: %s.\n", input.c_str());
+
+        const size_t colCount = 16;
+        size_t row = 0, col = 0;
+        size_t index = 0;
+        char c = ifs.get();
+
+        while (ifs.good()) {
+            if ( 0 == col ) {
+                //printf("[%10d][%10d]", row, index);
+                printf("[%10d]", row);
+            }
+            printf(" %02X", c);
+            c = ifs.get();
+            ++index;
+            ++col;
+
+            if ( colCount == col ) {
+                printf("\n");
+                col = 0;
+                ++row;
+            }
+        }
+        printf("\n");
+        printf("total size=%d\n", index);
+
+        ifs.close();
+    }
+    else {
+        result = false;
+        printf("Error! Failed to open input file: %s\n", input.c_str());
+    }
+
+    return result;
+}
+
 
 int CCALL main ( int argc, /*const*/ char * argv[], /*const*/ char* /*const*/ * envp )
 {
@@ -83,8 +191,16 @@ int CCALL main ( int argc, /*const*/ char * argv[], /*const*/ char* /*const*/ * 
         ("plugins,p", "enable plugins system")
     #endif
         ("printCompliedInfo", "print complied information")
+        ("verbose", po::value<int>()->default_value(0)->implicit_value(1), "verbosity level")
+        ("conversion-mode,c", po::value<std::string>()->default_value("hex2bin")->implicit_value("hex2bin"), "convert hex string to bin")
+        ("input,i" , po::value<std::string>()->default_value("")->implicit_value(""), "input")
+        ("output,o", po::value<std::string>()->default_value("")->implicit_value(""), "output")
     ;
 
+    bool verbose = false;
+    std::string conversionMode;
+    std::string input;
+    std::string output;
     po::variables_map vm;
     try {
         po::store(po::parse_command_line(argc, argv, desc), vm);
@@ -137,7 +253,72 @@ int CCALL main ( int argc, /*const*/ char * argv[], /*const*/ char* /*const*/ * 
             printf("libsysinfo_isCharSigned=%d\n", libsysinfo_isCharSigned());
         }
 
+        if ( vm.count("verbose") ) {
+            verbose = vm["verbose"].as<int>() != 0;
+            printf("verbose=%d\n", verbose?1:0);
+        }
+
+
+        if ( vm.count("conversion-mode") ) {
+            conversionMode = vm["conversion-mode"].as<std::string>();
+        }
+
+
+        if ( vm.count("input") ) {
+            input = vm["input"].as<std::string>();
+        }
+
+
+        if ( vm.count("output") ) {
+            output = vm["output"].as<std::string>();
+        }
+
+
         printf(">>  Appliction Begin\n");
+
+
+        if ( !input.empty() ) {
+            if ( '=' == input[0] ) {
+                input.erase(0, 1);
+            }
+            printf("input=%s\n", input.c_str());
+        }
+
+
+        if ( !output.empty() ) {
+            printf("output=%s\n", output.c_str());
+        }
+
+
+        if ( !conversionMode.empty() ) {
+            if ( '=' == conversionMode[0] ) {
+                conversionMode.erase(0, 1);
+            }
+
+            printf("conversion-mode=%s\n", conversionMode.c_str());
+            bool result = true;
+            if ( !input.empty() ) {
+                if( 0 == strcmp(conversionMode.c_str(), "hex2bin") ) {
+                    result = convertHex2Bin(input, output, verbose);
+                }
+                else if( 0 == strcmp(conversionMode.c_str(), "bin2hex") ) {
+                    result = convertBin2Hex(input, output, verbose);
+                }
+                else {
+                    result = false;
+                    printf("Error! There is invalid parameter '--conversion-mode=%s'.\n", conversionMode.c_str());
+                }
+            }
+            else {
+                result = false;
+                printf("Error! There is invalid parameter '--input' (empty string).\n");
+            }
+
+            if ( result ) {
+                printf("Conversion OK!\n");
+            }
+        }
+
 
         DoSomethingInMainBody();
 
